0b438f306c11c4c59c3ae5eb496d7e224a94f53e
Johan Rossi <johanfrossi@gmail.com>
2025-09-08 20:01:35 -0300

    Adição de alguns itens

---
+++ secaotimes/times/clashofclans.php
@@ -0,0 +1,59 @@
+<?php include('../includes/theader.php'); ?>
+
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>CLASH OF CLANS</title>
+    <link rel="stylesheet" href="time.css">
+</head>
+<body>
+
+<main>
+
+    <!-- Descrição do time -->
+    <section class="descricao-time">
+        <h2>Sobre o Time</h2>
+        <p>The Last Hope monta seu time para o Clash Of Clans apenas para conteudo e passa tempo.</p>
+    </section>
+
+    <!-- Jogadores -->
+    <section class="jogadores">
+        <h2>Jogadores</h2>
+        <a href="../../secaojogadores/jogadores/pokas.php" class="jogador">
+            <img src="../img/pokas2.png" alt="Jogador 1">
+            <h3>PokasZideia</h3>
+            <p>CV 10, 9, 8, 8</p>
+</a>        
+        <a  class="jogador">
+            <img src="../img/semfoto.png" alt="Jogador 1">
+            <h3>Eric</h3>
+            <p>CV 12</p>
+</a>
+    </section>
+
+    <section class="jogadores">
+        <h2>Assistência Técnica</h2>
+    </section>
+
+    <!-- Partidas -->
+    <section class="partidas">
+        <h2>Guerras Recentes</h2>
+        <ul>
+            <li>TLH 0 X 0 **** | 5x5 | DIA DE PREPARAÇÃO</li>
+            <li>TLH 15 X 12 нолик | 5x5 | VITORIA</li>
+            <li>TLH 11 X 14 kongu boizz | 5x5 | DERROTA</li>
+        </ul>
+    </section>
+
+    <!-- Campeonatos -->
+    <section class="campeonatos">
+        <h2>Ligas</h2>
+        <ul>
+            <li>Não existe campeonatos disputados.</li>
+        </ul>
+    </section>
+
+</main>
+
+</body>
+</html>
